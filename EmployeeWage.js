e59b0e32a0188285f360a1f4c695d4f1e3900f6c
Vishal@7038 <vishalrathod7038@gmail.com>
2021-12-04 12:03:35 +0530

    [vishal] Cheacked Employee Present Or Absent

---
+++ EmployeeWage.js
@@ -0,0 +1,11 @@
+//constant variable
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random()*10) % 2;
+if (empCheck == IS_ABSENT)
+{
+    console.log("Employee is Absent");
+}
+else 
+{
+    console.log("Employee is Present");
+}
